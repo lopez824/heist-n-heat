253bc326da2ac6bc4316c106109141f2bf671855
lopez824 <lionelslopez14@gmail.com>
2021-09-12 12:00:35 -0600

    Upgraded to Webpack 5 for potential features

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import Phaser from "phaser";
+import logoImg from "./assets/logo.png";
+import "./index.css"
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
+class MyGame extends Phaser.Scene {
+  constructor() {
+    super();
+  }
 
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+  preload() {
+    this.load.image("logo", logoImg);
+  }
+
+  create() {
+    const logo = this.add.image(400, 150, "logo");
+
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1,
+    });
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'gameContainer',
-    width: 800,
-    height: 600,
-    scene: MyGame,
-    canvas: document.querySelector('canvas')
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: MyGame,
+  parent: "gameContainer"
 };
 
 const game = new Phaser.Game(config);
