46ed6bc309d0bfd8f7ee752da748de07d323af98
lopez824 <lionelslopez14@gmail.com>
2021-09-13 11:44:25 -0600

    Added Favicon with a new plugin

--- webpack/base.js
+++ webpack/base.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -42,6 +43,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
+    new FaviconsWebpackPlugin('./src/assets/favicon.png'),
   ],
   output: {
     publicPath: '/'
