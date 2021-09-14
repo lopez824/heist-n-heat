04f10c08c09c8c3bedddebad620133e29e0d3bf0
lopez824 <lionelslopez14@gmail.com>
2021-09-13 19:49:18 -0600

    Added Flag Collection and score

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import Phaser from "phaser";
 import mapImg from "./assets/mapExample.png";
-import playerImg from "./assets/Car_Placeholder.png"
+import playerImg from "./assets/Car_Placeholder.png";
+import flagImg from './assets/SpecialFlag.png';
 import "./index.css"
 
 var player;
+var flags;
 var cursors;
 var text;
+var score;
 
 class MyGame extends Phaser.Scene {
 
@@ -16,58 +19,81 @@ class MyGame extends Phaser.Scene {
   preload() {
     this.load.image("map", mapImg);
     this.load.image("player", playerImg);
+    this.load.image("flag", flagImg);
   }
 
   create() {
-    const map = this.add.image(400, 300,"map");
+    const map = this.add.image(400, 320,"map");
     map.scale = map.scale*1.75;
 
-    player = this.physics.add.image(400,300,"player");
+    const flag1 = this.physics.add.image(248, 210,'flag');
+    flag1.scale = flag1.scale/4;
+    const flag2 = this.physics.add.image(552, 310,'flag');
+    flag2.scale = flag2.scale/4;
+    const flag3 = this.physics.add.image(430, 420,'flag');
+    flag3.scale = flag3.scale/4;
+
+    player = this.physics.add.image(400,320,"player");
     player.scale = player.scale/2;
     player.setDamping(true);
-    player.setCollideWorldBounds(true);
+    //player.setCollideWorldBounds(true);
     player.setMaxVelocity(300);
     cursors = this.input.keyboard.createCursorKeys();
-    text = this.add.text(10,10,'', {font: '16px Courier', fill: '#00ff00'});
+    text = this.add.text(10,10,'', {font: '16px Courier', fill: '#ffffff'});
+    score = 0;
+
+    this.physics.add.collider(player, flag1, function (player,flag1) {
+        score += 100;
+        flag1.destroy();
+    }); 
+    this.physics.add.collider(player, flag2, function (player,flag2) {
+      score += 100;
+      flag2.destroy();
+    }); 
+    this.physics.add.collider(player, flag3, function (player,flag3) {
+      score += 100;
+      flag3.destroy();
+    }); 
   }
 
   update() {
-    player.setAcceleration(0);
-    player.setDrag(0);
+    player.setVelocity(200,200);
+    //player.setAcceleration(0);
+    //player.setDrag(0);
     player.body.angularVelocity = 0;
 
     if (cursors.up.isDown) {
-      player.body.acceleration.setToPolar(player.rotation, 300);
+      //player.body.acceleration.setToPolar(player.rotation, 300);
     } 
     else if (cursors.down.isDown) {
-      player.setDrag(0.25);
+      //player.setDrag(0.25);
     } 
     else {
-      player.setDrag(0.25);
+      //player.setDrag(0.25);
     }
 
     if (cursors.left.isDown)
     {
       player.rotation -= .03;
-      player.setVelocityY(150);
-      this.time.addEvent({ delay: 100, callback: () => player.body.speed += 5});
+      player.setVelocityY(100);
+      this.time.addEvent({ delay: 100, callback: () => player.setDrag(.01)});
     }
     else if (cursors.right.isDown)
     {
       player.rotation += .03;
-      player.setVelocityY(150);
-      this.time.addEvent({ delay: 100, callback: () => player.body.speed += 5});
+      player.setVelocityY(100);
+      this.time.addEvent({ delay: 100, callback: () => player.setDrag(.01)});
     }
 
     this.physics.velocityFromRotation(player.rotation, player.body.speed, player.body.velocity);
 
-    if (!cursors.up.isDown && player.body.speed < 3) {
+    /* if (!cursors.up.isDown && player.body.speed < 3) {
       // Come to a full stop.
       // This also stops rotation.
       player.body.stop();
-    }
+    } */
 
-    text.setText('Speed: ' + player.body.speed);
+    text.setText('Score: ' + score);
     this.physics.world.wrap(player, 32);
   }
 }
