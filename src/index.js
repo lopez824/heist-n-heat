4f47822d4b7831b520f5ee54f200669458516e19
lopez824 <lionelslopez14@gmail.com>
2021-09-15 21:56:39 -0600

    organized code and adjusted player speed

--- src/index.js
+++ src/index.js
@@ -27,38 +27,42 @@ class MyGame extends Phaser.Scene {
 
   create() {
     
+    // initialize map
     this.cameras.main.setBounds(0, 0, 2150, 2525);
     const map = this.add.image(850, 1000,"map");
     map.scale = map.scale*3;
-
     const cash1 = this.matter.add.sprite(230, 210,'cash').setStatic(true).setSensor(true);
     const cash2 = this.matter.add.sprite(552, 175,'cash').setStatic(true).setSensor(true);
     const cash3 = this.matter.add.sprite(430, 450,'cash').setStatic(true).setSensor(true);
 
+    //initialize player
     player = this.matter.add.sprite(400,320,"player");
-    player.body.gameObject.name = "Player";
-    player.setBody({
+    player.setBody({    // adjust player hitbox size
       type: 'rectangle',
       width: 90,
       height: 50
     });
+
+    // matterJS properties
     player.setScale(0.5);
     player.setMass(30);
     player.setFrictionAir(0.1);
     player.setFixedRotation();
     
+    // initialize User Interface
+    text = this.add.text(10,0,'', {font: '16px Courier', fill: '#ffffff'}).setScrollFactor(0);
+    score = 0;
+    var graphics = new Phaser.Geom.Rectangle(0, 30, 150, 25);
+    fuel = this.add.graphics({ fillStyle: { color: 0x00ff00 } }).setScrollFactor(0);
+    fuel.fillRectShape(graphics);
+    debugText = this.add.text(10,20,'', {font: '16px Courier', fill: '#ffffff'});
+
+    // set up inputs
     cursors = this.input.keyboard.createCursorKeys();
     keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    debugText = this.add.text(10,20,'', {font: '16px Courier', fill: '#ffffff'});
-    text = this.add.text(10,0,'', {font: '16px Courier', fill: '#ffffff'}).setScrollFactor(0);
 
-    var fuel = new Phaser.Geom.Rectangle(0, 30, 150, 25);
-    var graphics = this.add.graphics({ fillStyle: { color: 0x00ff00 } }).setScrollFactor(0);
-    graphics.fillRectShape(fuel);
-
-    score = 0;
-
+    // collision listeners
     this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
         if (bodyA.gameObject.texture.key == "cash" && bodyB.gameObject.texture.key == "player") {
           score += 100;
@@ -66,42 +70,28 @@ class MyGame extends Phaser.Scene {
         }
     });
 
+    // tween animations
     this.tweens.add({
-      targets: graphics,
+      targets: fuel,
       scaleX: .01,
       ease: 'Linear',
       duration: 30000
     });
-    this.matter.world.setBounds(0, 0, 2150, 2525);
+
+    this.matter.world.setBounds(0, 0, 2150, 2525);  // sets game world bounds
   }
 
   update() {
     this.cameras.main.centerOn(player.x,player.y);
-    player.thrust(0.03);
-    
-    if (cursors.up.isDown) {
-      //player.body.acceleration.setToPolar(player.rotation, 300);
-    } 
-    else if (cursors.down.isDown) {
-      //player.setDrag(0.25);
-    } 
-    else {
-      //player.setDrag(0.25);
-    }
+    player.thrust(0.06);
 
     if (cursors.left.isDown || keyA.isDown)
     {
-      player.angle -= 2.5;
-      //player.thrust(0.005);
-      //player.setVelocityY(100);
-      //this.time.addEvent({ delay: 100, callback: () => player.setDrag(.01)});
+      player.angle -= 5;
     }
     else if (cursors.right.isDown || keyD.isDown)
     {
-      player.angle += 2.5;
-      //player.thrust(0.005);
-      //player.setVelocityY(100);
-      //this.time.addEvent({ delay: 100, callback: () => player.setDrag(.01)});
+      player.angle += 5;
     }
 
     text.setText('Score: $' + score);
